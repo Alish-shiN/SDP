b3d02a7a62d989bebe5526864e2afc676f0e3c5f
Alish-shiN <alisherkulataev860@gmail.com>
2024-09-29 21:13:53 +0500

    Add files via upload

---
+++ src/AbstractFactory/MacButton.java
@@ -0,0 +1,7 @@
+package AbstractFactory;
+
+public class MacButton implements Button {
+    public void click() {
+        System.out.println("Mac button clicked.");
+    }
+}
---
+++ src/Prototype/CloneableProduct.java
@@ -0,0 +1,6 @@
+package Prototype;
+
+public interface CloneableProduct extends Cloneable {
+    CloneableProduct cloneProduct();
+}
+
